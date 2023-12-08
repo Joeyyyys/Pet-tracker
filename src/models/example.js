acd5debb397b952759f65d6887673c4365e1df78
Joeyyyys <josephgiler2@gmail.com>
2023-12-08 14:32:42 -0500

    update my Pets class inside my models folder so that I am able to use those class methods in my express files

--- src/models/example.js
+++ src/models/example.js
@@ -1,17 +1,45 @@
 const knex = require('./knex');
 
-class Example {
+class Pets {
   // This is just an example query, obviously you need to change it, but it shows you how to use knex.raw and dynamic values
-  static async create(data) {
+  static async create(petData) {
     try {
-      const query = `INSERT INTO examples (data) values (?) returning *`;
-      const { rows: [newToDo] } = await knex.raw(query, [data]);
-      return newToDo;
+      const query = `INSERT INTO pets (pet_name, profile_picture, species, is_friendly) values (?, ?, ?, ?) returning *`;
+      const { rows: [newPet] } = await knex.raw(query, [
+        petData.pet_name,
+        petData.profile_picture,
+        petData.species,
+        petData.is_friendly || false,
+      ]);
+      return newPet;
+    } catch (err) {
+      console.error(err);
+      return null;
+      
+    }
+  }
+
+  static async list() {
+    try {
+      const query = 'SELECT * FROM pets';
+      const response = await knex.raw(query);
+      return response.rows || [];
     } catch (err) {
       console.error(err);
       return null;
     }
   }
+
+  static async remove(petId) {
+    try {
+        const query = 'DELETE FROM pets WHERE id = ? RETURNING *';
+        const { rows: [removedPet] } = await knex.raw(query, [petId]);
+        return removedPet;
+      } catch (err) {
+        console.error(err);
+        return null;
+      }
+  }
 }
 
-module.exports = Example;
+module.exports = Pets;
